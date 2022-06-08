80f2b6c26b1147a606c0333aeeb53ec0f3bdfd22
Enocki229 <enock@urban-technology.net>
2022-06-08 16:12:02 +0100

    Update uBrand.php

--- src/Service/uBrand.php
+++ src/Service/uBrand.php
@@ -22,10 +22,9 @@ class uBrand extends AbstractController
     public function index(){
         $search_brand     = $this->brandRepository->findOneBy(['siteUrl' => 'https://'.$_SERVER['HTTP_HOST']]);
         $brand        = ($search_brand) ? $search_brand : $this->brandRepository->findOneBy(['siteUrl' => 'http://'.$_SERVER['HTTP_HOST']]);
-        //dd($brand, $search_brand);
         $brandAdmin   = $brand->getManager();
         $company      = ($brandAdmin) ? $brandAdmin->getCompany() : $this->companyRepository->findOneBy(['id' => 1]);
-        dd($brandAdmin->getIsDlr(), $brandAdmin->getCompany(), $company);
+        //dd($brandAdmin->getIsDlr(), $brandAdmin->getCompany(), $company);
         
         return[
            'name'               => $brand->getName(),
