70a9e48d1f987dcd13a6168634ce28276e12fef5
Enocki229 <enock@urban-technology.net>
2022-06-16 12:33:24 +0100

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,10 +37,7 @@ class RegistrationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
             $user->setPassword(
-            $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
+            $userPasswordHasher->hashPassword($user, $form->get('plainPassword')->getData())
             );
 
             // A commenter revoir lorsque l'envoi des mail est activé
