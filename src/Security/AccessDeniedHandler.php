6c41039b592ff3902494e91ddc8b744f317d3762
Enocki229 <enock@urban-technology.net>
2022-06-29 13:05:34 +0100

    Update AccessDeniedHandler.php

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -5,13 +5,14 @@ namespace App\Security;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
 
 class AccessDeniedHandler implements AccessDeniedHandlerInterface {
     public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response {
         // ...
-
-        return new Response($content, 403);
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        //return new Response($content, 403);
     }
 }
