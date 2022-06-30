fe4b63ef4bffa8482f55d8ec3f4b62d5cedbc825
utech229 <utech229@gmail.com>
2022-06-30 04:20:36 -0400

    update to email bundles

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd8eb1f8e4f55eb77e1da20d57667094124d009cc',
+        'reference' => 'fed6627d0c0d7829dd0e5247e48471bf508c2cf2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd8eb1f8e4f55eb77e1da20d57667094124d009cc',
+            'reference' => 'fed6627d0c0d7829dd0e5247e48471bf508c2cf2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
