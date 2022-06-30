19204b7a0ecaebdde2be681934c907029787851b
Enocki229 <enock@urban-technology.net>
2022-06-30 09:16:50 +0100

    update for email

---
+++ src/Service/sMailer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\Services;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+
+
+class sMailer extends AbstractController
+{
+    protected $brand;
+   
+	public function __construct(TranslatorInterface $intl,Services $services, MailerInterface $mailer)
+	{
+       $this->intl    = $intl;
+       $this->services = $services;
+       $this->mailer = $mailer;
+    }
+
+    public function send(): Response 
+    {
+        $mailer = $this->mailer;
+        $email = (new Email())
+            ->from('support@zekin.app')
+            ->to('enockiatk@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            ->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+    }
+
+    
+}
