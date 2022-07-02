5d753930064c30954ce5eb1f31b1f8545043549f
Enocki229 <enock@urban-technology.net>
2022-07-02 07:54:56 +0100

    Update SuperController.php

--- src/Controller/SuperController.php
+++ src/Controller/SuperController.php
@@ -98,6 +98,10 @@ class SuperController extends AbstractController
                     $this->intl->trans("Insertion du tableau de données pays"),
                     $this->intl->trans("La recherche du nom du pays à échoué : BrickPhone"),
                 );
+                
+            $brand   = $this->brandRepository->findOneByName($this->brand->get()['name']);
+            $route   = $this->routerRepository->findOneByName("Fastermessage_moov");
+            $company = $this->companyRepository->findOneById(1);
 
             $user->setRole($role);
             $user->setRoles(['ROLE_'.$role->getName()]);
@@ -119,6 +123,9 @@ class SuperController extends AbstractController
             $user->setPassword(
             //encode the plain password
             $userPasswordHasher->hashPassword($user, '@21061AdminDefault'));
+            $user->setAccountManager($user)
+                ->setBrand($brand)
+                ->setRouter($route);
             $this->userRepository->add($user);
             $this->AddEntity->defaultUsetting($user,  [
                 'ccode' => 'XOF',
@@ -126,15 +133,6 @@ class SuperController extends AbstractController
                 'ufirstname' => 'Bill',
                 'ulastname'  => 'FASSINOU'
             ]);
-
-            $brand   = $this->brandRepository->findOneByName($this->brand->get()['name']);
-            $route   = $this->routerRepository->findOneByName("Fastermessage_moov");
-            dd($route);
-            $company = $this->companyRepository->findOneById(1);
-            $user->setAccountManager($user)
-                ->setBrand($brand)
-                ->setRouter($route);
-            $this->userRepository->add($user);
             return $this->services->msg_success(
                 $this->intl->trans("Création du super admin : SUP-ONE"),
                 $this->intl->trans("Utilisateur SUP-ONE ajouté avec succès")
