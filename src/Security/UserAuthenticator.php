33d75addcf4c80dc535a6bd611a02488fff2057c
Enocki229 <enock@urban-technology.net>
2022-07-02 07:44:35 +0100

    Update UserAuthenticator.php

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -24,6 +24,7 @@ use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 
