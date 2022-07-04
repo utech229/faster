a88b38ed1bca0f156d0656f224df4e981d402894
codealley6 <faissale@urban-technology.net>
2022-07-04 13:51:06 +0100

    Update ContactGroup
    Update ContactGroup

--- public/app/js/queries/group-contact/list.js
+++ public/app/js/queries/group-contact/list.js
@@ -148,7 +148,7 @@ var KTGroupList = function() {
                                     confirmButton: "btn fw-bold btn-primary"
                                 }
                             })  :
-                            window.location.assign(url_campaign+'?grps='+tabUid+'&user=qcAe6S0qWfl02Is8YbZlAq6yURQBtD');
+                            window.location.assign(url_campaign+'?grps='+tabUid+'&user='+user);
                         }
                         else{
                             "cancel" === t.dismiss && $(document).trigger('onAjaxInfo');
