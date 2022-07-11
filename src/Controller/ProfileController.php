d00a58857469567de3a535f598e197de905ac836
Enocki229 <enock@urban-technology.net>
2022-07-11 11:08:07 +0100

    Update ProfileController.php

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -183,7 +183,7 @@ class ProfileController extends AbstractController
             if ($userPasswordHasher->isPasswordValid($user, $password))
             {
                 //Validation on Linksettingcontroller : app_user_email_setting
-                $user->setActiveCode($this->services->numericgenerate(6));
+                $user->setActiveCode($this->services->numeric_generate(6));
                 $user->setUpdatedAt(new \DatetimeImmutable());
                 $this->userRepository->add($user);
                 // Lien de réinitialisation
