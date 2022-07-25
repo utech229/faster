f16b1cc16e72dddd6a8f1a78ac14c2682438ac63
Enocki229 <enock@urban-technology.net>
2022-07-25 12:02:11 +0100

    Update UsersUploadController.php

--- src/Controller/UsersUploadController.php
+++ src/Controller/UsersUploadController.php
@@ -232,6 +232,7 @@ class UsersUploadController extends AbstractController
                     $user->setPrice([
                         $countryDatas['code'] => $priceDatas,
                     ]);
+                    dd($email);
                     $user->setStatus($this->services->status(3));
                     $user->setRouter($this->routeRepository->findOneByName('FASTERMESSAGE_MOOV'));
                     $user->setBrand($this->brandRepository->findOneByName('FASTERMESSAGE'));
